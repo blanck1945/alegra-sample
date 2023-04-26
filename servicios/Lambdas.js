d4e8dd392bb8fcc53869b14f7913f765a49207da
apastrana-auntap <apastrana@auntap.com>
2023-04-26 01:37:44 -0300

    Change url

--- servicios/Lambdas.js
+++ servicios/Lambdas.js
@@ -13,7 +13,8 @@ class LambdasService {
     await axios({
       method: "post",
       //url: "http://localhost:3006/dev",
-      url: "https://erocq81xb0.execute-api.us-east-2.amazonaws.com/dev/upload-alegra",
+      // url: "https://erocq81xb0.execute-api.us-east-2.amazonaws.com/dev/upload-alegra",
+      url: "https://alegra-be-upload.onrender.com/upload-alegra",
       data: formattedBills,
     });
   }
