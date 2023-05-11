1ae799d39308cc0fc9b27fefb5af63234bf8c614
apastrana-auntap <apastrana@auntap.com>
2023-05-10 21:21:32 -0300

    Update url

--- servicios/Lambdas.js
+++ servicios/Lambdas.js
@@ -14,7 +14,8 @@ class LambdasService {
       method: "post",
       //url: "http://localhost:3008/upload-alegra",
       // url: "https://erocq81xb0.execute-api.us-east-2.amazonaws.com/dev/upload-alegra",
-      url: "https://alegra-be-upload.onrender.com/upload-alegra",
+      //url: "https://alegra-be-upload.onrender.com/upload-alegra",
+      url: "https://alegra-be-upload-production.up.railway.app/upload-alegra",
       data: formattedBills,
     });
   }
